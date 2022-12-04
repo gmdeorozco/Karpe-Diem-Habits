44d18db5503e44155df01773ef75a18c5770d048
gmdeorozco <ernesto.orozco.coulson@gmail.com>
2022-12-04 04:31:57 +0000

    delete repeated service

--- tasks-server/src/main/java/com/KarpeDiemHabits/ChallengesServer/repository/DayLifeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.KarpeDiemHabits.ChallengesServer.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.KarpeDiemHabits.ChallengesServer.entities.DayLife;
-
-public interface DayLifeRepository extends CrudRepository < DayLife , Long > {
-    
-}
