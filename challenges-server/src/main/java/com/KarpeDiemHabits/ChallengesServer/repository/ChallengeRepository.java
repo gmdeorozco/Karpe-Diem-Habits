a9e9518f59707df6327406e2875b6c1022f13158
gmdeorozco <ernesto.orozco.coulson@gmail.com>
2022-12-04 09:17:20 +0000

    commit

--- challenges-server/src/main/java/com/KarpeDiemHabits/ChallengesServer/repository/ChallengeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.KarpeDiemHabits.ChallengesServer.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.KarpeDiemHabits.ChallengesServer.entities.Challenge;
-
-public interface ChallengeRepository extends CrudRepository < Challenge, Long >{
-    
-}
